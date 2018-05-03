6862b82d140332e64534632e13e5449c7670b2a1
Andres, Erythrina Nicole <2160051@slu.edu.ph>
2018-05-04 00:16:41 +0800

    Fixed scoring system for quiz

--- Projects/Finals Website/php/index.php
+++ Projects/Finals Website/php/index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<html>
     <head>
         <meta name=viewport content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <link href="../styles/quizStyle.css" rel="stylesheet" type="text/css"/>
@@ -19,28 +19,31 @@
             echo '<li>
                     <h3>'. $quizQuestion['question']. '</h3> 
                     <div>
-                        <input type="radio" name="answerOne" id="answerOne" value="a" />
+                        <input type="radio" name='.$counter.' id='.$counter.' value="a" />
                         <label for="answerOneA">A)'. $quizQuestion['choices'][0]. '</label>
                     </div>
         
                     <div>
-                        <input type="radio" name="answerOne" id="answerOne" value="b" />
+                        <input type="radio" name='.$counter.' id='.$counter.' value="b" />
                         <label for="answerOneB">B)'. $quizQuestion['choices'][1]. '</label>
                     </div>
         
                     <div>
-                        <input type="radio" name="answerOne" id="answerOne" value="c" />
+                        <input type="radio" name='.$counter.' id='.$counter.' value="c" />
                         <label for="answerOneC">C)'. $quizQuestion['choices'][2]. '</label>
                     </div>
 
                     <div>
-                        <input type="radio" name="answerOne" id="answerOne" value="d" />
+                        <input type="radio" name='.$counter.' id='.$counter.' value="d" />
                         <label for="answerOneD">D)'. $quizQuestion['choices'][3].'</label>
                     </div>
                 </li>';
+                $counter++;
         }
         echo "</ol>
-                <input type='submit' value='Submit Quiz' class='submitButton'/>
+                <input type='submit' value='Submit' class='submitButton' />
              </form>";
 ?>
 </div>
+</body>
+</html>
--- Projects/Finals Website/php/process.php
+++ Projects/Finals Website/php/process.php
@@ -2,38 +2,36 @@
 <head>
     <head>
         <meta name=viewport content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-        <link href="quizStyle.css" rel="stylesheet" type="text/css"/>
+        <link href="../styles/quizStyle.css" rel="stylesheet" type="text/css"/>
     </head>
-<?php 
-
-	$fid = $_GET['id'];
-
-?>
 <body>
 
 <div id="wrapper">
-
-<center>Your Score</font></center>
 <br />
 <br />
 
 <?php
-	$answer1= $_POST['answerOne'];
-	$answer2= $_POST['answerTwo'];
-	$answer3= $_POST['answerThree'];
+	$url = '../json/questions.json'; 
+    $data = file_get_contents($url); 
+    $quizQuestions = json_decode($data, true);
+	$counter = 0;
 	$score = 0;
 	
-	if ($answer1 == "A"){$score++;}
-	if ($answer2 == "B"){$score++;}
-	if ($answer3 == "C"){$score++;}
-	echo "<center><font face='Berlin Sans FB' size='8'>Your Score is <br> $score/3</font></center>";
+	foreach ($quizQuestions as $quizQuestion) {
+		$studentAnswer = $_POST[$counter];		
+
+		if($studentAnswer == $quizQuestion['answer']){
+			$score++;
+		}
+		$counter++;
+	}
+	echo "<center><font face='Berlin Sans FB' size='8'>Your Score is <br> $score/$counter <br></font></center>";
 	
 ?>
 
 
 
 
-</div><!--- end of wrapper div --->
-
+</div>
 </body>
 </html>
