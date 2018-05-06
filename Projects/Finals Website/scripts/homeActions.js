80b525a0b7c19b4a82f3f86b3f9be1257eb92446
Britanny Baldovino <2161322@slu.edu.ph>
2018-05-06 20:08:44 +0800

    Added javascript files

---
+++ Projects/Finals Website/scripts/homeActions.js
@@ -0,0 +1,45 @@
+var light = document.getElementById('lightbulb1');
+light.style.filter = "grayscale(" + 100 + "%)";
+function turnOn() {
+	if(light.style.filter != "grayscale(" + 100 + "%)"){
+        light.setAttribute("style","filter:grayscale(" + 100 + "%)");
+	}else{
+		light.setAttribute("style","filter:grayscale(" + 0 + "%)");
+	}
+}
+var light1 = document.getElementById('lightbulb2');
+light1.style.filter = "grayscale(" + 100 + "%)";
+function turnOn1() {
+	if(light1.style.filter != "grayscale(" + 100 + "%)"){
+        light1.setAttribute("style","filter:grayscale(" + 100 + "%)");
+	}else{
+		light1.setAttribute("style","filter:grayscale(" + 0 + "%)");
+	}
+}
+var light2 = document.getElementById('lightbulb3');
+light2.style.filter = "grayscale(" + 100 + "%)";
+function turnOn2() {
+	if(light2.style.filter != "grayscale(" + 100 + "%)"){
+        light2.setAttribute("style","filter:grayscale(" + 100 + "%)");
+	}else{
+		light2.setAttribute("style","filter:grayscale(" + 0 + "%)");
+	}
+}
+var light3 = document.getElementById('lightbulb4');
+light3.style.filter = "grayscale(" + 100 + "%)";
+function turnOn3() {
+	if(light3.style.filter != "grayscale(" + 100 + "%)"){
+        light3.setAttribute("style","filter:grayscale(" + 100 + "%)");
+	}else{
+		light3.setAttribute("style","filter:grayscale(" + 0 + "%)");
+	}
+}
+var light4 = document.getElementById('lightbulb5');
+light4.style.filter = "grayscale(" + 100 + "%)";
+function turnOn4() {
+	if(light4.style.filter != "grayscale(" + 100 + "%)"){
+        light4.setAttribute("style","filter:grayscale(" + 100 + "%)");
+	}else{
+		light4.setAttribute("style","filter:grayscale(" + 0 + "%)");
+	}
+}
---
+++ Projects/Finals Website/scripts/sideActions.js
@@ -0,0 +1,10 @@
+function openSide() {
+	document.getElementById("sideNav").style.left = "0px";
+	document.getElementById("bottomNav").style.left = "0px";
+	document.getElementById("sideOpener").style.left = "290px";
+}
+function closeSide() {
+	document.getElementById("sideNav").style.left = "-300px";
+	document.getElementById("bottomNav").style.left = "-300px";
+	document.getElementById("sideOpener").style.left = "-290px";
+}
