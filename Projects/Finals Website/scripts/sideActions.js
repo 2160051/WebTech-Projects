b2c612e1f3e44e98aab526aceb751a262ee88dc3
Britanny Baldovino <2161322@slu.edu.ph>
2018-05-12 01:11:39 +0800

    Added extra tabs for webpage features

--- Projects/Finals Website/scripts/sideActions.js
+++ Projects/Finals Website/scripts/sideActions.js
@@ -8,3 +8,11 @@ function closeSide() {
 	document.getElementById("bottomNav").style.left = "-300px";
 	document.getElementById("sideOpener").style.left = "-290px";
 }
+function openSides() {
+	document.getElementById("sidebits").style.right = "0px";
+	document.getElementById("resourcescloser").style.right = "795px";
+}
+function closeSides() {
+	document.getElementById("sidebits").style.right = "-800px";
+	document.getElementById("resourcescloser").style.right = "-795px";
+}
