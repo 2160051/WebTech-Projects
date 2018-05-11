7b21da2b18de4d79bf5d98515ad50be86756b3f4
Britanny Baldovino <2161322@slu.edu.ph>
2018-05-12 03:56:19 +0800

    Added quiz cute tab

--- Projects/Finals Website/scripts/sideActions.js
+++ Projects/Finals Website/scripts/sideActions.js
@@ -16,3 +16,11 @@ function closeSides() {
 	document.getElementById("sidebits").style.right = "-800px";
 	document.getElementById("resourcescloser").style.right = "-795px";
 }
+function sopenSides() {
+	document.getElementById("quizBox").style.right = "0px";
+	document.getElementById("quizcloser").style.right = "395px";
+}
+function scloseSides() {
+	document.getElementById("quizBox").style.right = "-400px";
+	document.getElementById("quizcloser").style.right = "-400px";
+}
